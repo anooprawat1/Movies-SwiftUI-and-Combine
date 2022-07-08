994c59f689fc97c514be0f99cae7b603e4d5d372
Anoop <anoop@b7ce4c90-302c-4894-b0ba-14c65e5b3e83.fritz.box>
2022-07-08 19:50:02 +0200

    Added project

---
+++ Movies-SwiftUI-and-Combine/Movies_SwiftUI_and_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movies_SwiftUI_and_CombineApp.swift
+//  Movies-SwiftUI-and-Combine
+//
+//  Created by Anoop on 08.07.22.
+//
+
+import SwiftUI
+
+@main
+struct Movies_SwiftUI_and_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
